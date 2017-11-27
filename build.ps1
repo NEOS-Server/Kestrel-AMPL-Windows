0b07409f5ecbaf2b384c91c07f2fde9c63a5de27
Ben Huebner <ben@huebner.us>
2017-11-27 13:30:22 -0600

    add powershell build script

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿# Cleanup build and dist directories
+Remove-Item -Force -Recurse .\dist
+Remove-Item -Force -Recurse .\build
+
+# Copy kestrel file from linux client repo
+Copy-Item -Force .\Kestrel-AMPL-Linux\kestrel .\kestrel.py
+
+# Run py2exe setup script
+python setup.py py2exe
+
+# Create zip file
+Compress-Archive -Path .\dist\*, .\Kestrel-AMPL-Linux\kestrelkill, .\Kestrel-AMPL-Linux\kestrelret, .\Kestrel-AMPL-Linux\kestrelsub -DestinationPath .\kestrel.zip
